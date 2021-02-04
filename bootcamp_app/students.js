ab0b47ff7742cef6e85fce45166052151f451c9a
Louis Coreas <l.g.coreas@gmail.com>
2021-02-04 23:20:59 +0000

    Update students.js and teachers.js to use parameterized queries.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -43,17 +43,39 @@ const pool = new Pool({
 //   })
 // });
 
-// this will allow user to input from terminal directly using process.argv
-pool
-  .query(
-    `
+// this will allow user to input from terminal directly using process.argv but also allows for malicious attacks
+// pool
+//   .query(
+//     `
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `
+//   )
+//   .then((res) => {
+//     res.rows.forEach((user) => {
+//       console.log(
+//         `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+//       );
+//     });
+//   })
+//   .catch((err) => console.error("query error", err.stack));
+
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`
-  )
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+pool.query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
@@ -62,5 +84,3 @@ LIMIT ${process.argv[3] || 5};
     });
   })
   .catch((err) => console.error("query error", err.stack));
-
-  
